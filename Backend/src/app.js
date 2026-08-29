cdd108d4332681d3f01faff8bb05738317399e97
pushpeshthakur <pushpeshthakur1626@gmail.com>
2026-08-30 00:27:46 +0530

    Frontenf url fixed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,13 +9,13 @@ import cartRouter from './routes/cartRoute.js';
 import addressRouter from './routes/addressRouter.js';
 import orderRouter from './routes/orderRoute.js';
 
-
+const allowedOrigins = ['http://localhost:5173', process.env.FRONTEND_URL];
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: allowedOrigins,
     credentials: true
 }));
 
