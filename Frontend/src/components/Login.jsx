fbeb8503e2a354049dbcb28fac23ee864a3a0fca
pushpeshthakur <pushpeshthakur1626@gmail.com>
2026-08-30 00:45:49 +0530

    login withCredential

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -16,6 +16,8 @@ const Login = () => {
             event.preventDefault();
             const {data} = await axios.post(`/api/user/${state}`, {
                 name, email, password
+            },{
+                withCredentials: true
             });
 
             if(data.success){
