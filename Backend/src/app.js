aaa6e81f1af0c6777efab132643ae072d2e8ddc9
pushpeshthakur <pushpeshthakur1626@gmail.com>
2026-08-30 14:50:35 +0530

    cors error fixed in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,20 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests without an Origin header
-        // (Postman, server-to-server, etc.)
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        }
-
-        return callback(new Error('Not allowed by CORS'));
-    },
-    //origin: allowedOrigins,
+    origin: allowedOrigins,
     credentials: true
 }));
 
