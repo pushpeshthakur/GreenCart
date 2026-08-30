475aa63304f04b4b8d594384cca6885e7c00feeb
pushpeshthakur <pushpeshthakur1626@gmail.com>
2026-08-30 15:12:15 +0530

    cors error fixed in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: allowedOrigins,
+    origin: 'https://green-cart-omega-ten.vercel.app/',
     credentials: true
 }));
 
